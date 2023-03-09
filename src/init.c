533d89d0728c27629f4f8e52e6f39651f48c9de8
ron <rontekhne@users.noreply.github.com>
2023-03-08 21:41:28 -0300

    escondi o cursor quando o jogo estiver on.

--- src/init.c
+++ src/init.c
@@ -43,6 +43,9 @@ void initSDL(void)
         exit(EXIT_FAILURE);
     }
 
+    /* desabilita o cursor */
+    SDL_ShowCursor(SDL_DISABLE);
+
     /* inicializa o suporte a imagens jpg e png */
     IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG);
 }
