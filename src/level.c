ba33538ade04f638a1d873d9002b47397ccd4a85
ron <rontekhne@users.noreply.github.com>
2023-03-10 19:26:15 -0300

    adicionado inimigo

--- src/level.c
+++ src/level.c
@@ -14,13 +14,18 @@ static void draw(void);
 static void initPlayer(void);
 static void throwPower(void);
 static void doPlayer(void);
+static void doFighters(void);
 static void doPower(void);
-static void drawPlayer(void);
+static void drawFighters(void);
 static void drawPower(void);
+static void spawnEnemies(void);
 
 static Entity *player;
 static SDL_Texture *powerTexture;
+static SDL_Texture *enemyTexture;
+static int enemySpawnTimer;
 
+/* inicializa a fase */
 void initLevel(void)
 {
     game.delegate.logic = logic;
@@ -33,8 +38,12 @@ void initLevel(void)
     initPlayer();
 
     powerTexture = loadTexture("gfx/playerPower.png");
+    enemyTexture = loadTexture("gfx/enemy.png");
+
+    enemySpawnTimer = 0;
 }
 
+/* inicializa o jogador */
 static void initPlayer(void)
 {
     player = malloc(sizeof(Entity));
@@ -48,12 +57,16 @@ static void initPlayer(void)
     SDL_QueryTexture(player->texture, NULL, NULL, &player->w, &player->h);
 }
 
+/* chama as funções de lógica */
 static void logic(void)
 {
     doPlayer();
+    doFighters();
     doPower();
+    spawnEnemies();
 }
 
+/* gerencia o comportamento do jogador */
 static void doPlayer(void)
 {
     player->dx = player->dy = 0;
@@ -77,10 +90,9 @@ static void doPlayer(void)
     if (game.keyboard[SDL_SCANCODE_LCTRL] && player->reload == 0) {
         throwPower();
     }
-    player->x += player->dx;
-    player->y += player->dy;
 }
 
+/* configura o poder do jogador */
 static void throwPower(void)
 {
     Entity *power;
@@ -101,6 +113,31 @@ static void throwPower(void)
     player->reload = 8;
 }
 
+/* gerencia a vida das entidades, jogador e inimigo */
+static void doFighters(void)
+{
+    Entity *e, *prev;
+
+    prev = &level.fighterHead;
+
+    for (e = level.fighterHead.next; e != NULL; e = e->next) {
+        e->x += e->dx;
+        e->y += e->dy;
+
+        if (e != player && e->x < -e->w) {
+            if (e == level.fighterTail) {
+                level.fighterTail = prev;
+            }
+
+            prev->next = e->next;
+            free(e);
+            e = prev;
+        }
+        prev = e;
+    }
+}
+
+/* gerencia a vida do poder do jogador */
 static void doPower(void)
 {
     Entity *p, *prev;
@@ -123,15 +160,41 @@ static void doPower(void)
     }
 }
 
+/* configura a aparição do inimigo */
+static void spawnEnemies(void)
+{
+    Entity *enemy;
+
+    if (--enemySpawnTimer <= 0) {
+        enemy = malloc(sizeof(Entity));
+        memset(enemy, 0, sizeof(Entity));
+        level.fighterTail->next = enemy;
+        level.fighterTail = enemy;
+
+        enemy->x = SCREEN_WIDTH;
+        enemy->y = rand() % SCREEN_HEIGHT;
+        enemy->texture = enemyTexture;
+        SDL_QueryTexture(enemy->texture, NULL, NULL, &enemy->w, &enemy->h);
+
+        enemy->dx = -(2 + (rand() % 4));
+        enemySpawnTimer = 30 + (rand() % 60);
+    }
+}
+
+/* chama as funções de desenho */
 static void draw(void)
 {
-    drawPlayer();
+    drawFighters();
     drawPower();
 }
 
-static void drawPlayer(void)
+static void drawFighters(void)
 {
-    blit(player->texture, player->x, player->y);
+    Entity *e;
+
+    for (e = level.fighterHead.next; e != NULL; e = e->next) {
+        blit(e->texture, e->x, e->y);
+    }
 }
 
 static void drawPower(void)
